913cd4211c96152fa2fb9ff2c11c309709c7c80c
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2024-10-01 22:18:59 +0800

    见zs/buding2所增

--- buding2/src/lib.rs
+++ buding2/src/lib.rs
@@ -233,28 +233,43 @@ pub extern fn base64_file__(env:&code_::Env_) -> Result2_ {
 
 #[no_mangle]
 pub extern fn exist_val__(env:&code_::Env_) -> Result2_ {
+/*fn for_val__(env:&code_::Env_, cmp:impl Fn(&str, &str) -> bool, only1:bool) -> Result2_ {*/
 	let q = as_ref__!(env.q);
 	let args = &as_ref__!(q.args_);
 	let a = &args.a_;
-	let mut idx = 0;
+	let mut val = String::new();
+
+	as_ref__!(a[0]).s__(&mut val);
+	let cntn = !val.is_empty();
+	val.clear();
+
+	as_ref__!(a[2]).s__(&mut val);
+	let only1 = !val.is_empty();
+	val.clear();
+
+	let mut idx = 4;
 	let up_q = q.up_.clone();
 	let mut q2 = up_q.clone();
-	let mut val = String::new();
 	let mut first = true;
 	while idx < a.len() {
 		let i = as_ref__!(a[idx]);
 		let mut b = false;
+		let mut next = true;
 		if i.dunhao__() {
 			b = true;
 		} else {
 			match code_::qv4rem__(&i.rem_, |_| {false}, q2.unwrap(), env.w.clone()) {
-				Ok(q3) => q2 = q3,
+				Ok(q3) => {
+					q2 = q3;
+					next = false;
+				}
 				Err(e) => return e,
 			}
 			i.s__(&mut val);
 		}
 		idx += 1;
-		if b || idx >= a.len() {
+		let mut b2 = idx >= a.len();
+		if b || b2 {
 			if first {first = false;} else {
 				as_ref__!(env.w).dunhao__(&mut as_mut_ref__!(env.ret));
 			}
@@ -269,22 +284,77 @@ pub extern fn exist_val__(env:&code_::Env_) -> Result2_ {
 					if q.vars_.get__(name, false, false, &mut ret2) {
 						if !ret2.is_empty() {
 							let s = ret2.s__();
-							if s == val {
+							if /*cmp(&s, &val)*/ if cntn {s.contains(&val)} else {s == val} {
 								let mut ret = as_mut_ref__!(env.ret);
 								let w = as_ref__!(env.w);
 								if has {
 									ret.add__(w.kws_.dunhao_.s_.clone());
 								}
-								ret.add__(w.text__(name));
+								if only1 {
+									ret.add__(name);
+									b2 = true;
+									return Some(());
+								} else {
+									ret.add__(w.text__(name));
+								}
 								has = true;
 							}
 						}
 					}
 				}
 				None
-			}, false, false, false);
+			}, next, false, false);
+
+			if(b2) {break;}
 
 			q2 = up_q.clone();
+			val.clear();
+		}
+	}
+	ok__()
+}
+/*#[no_mangle]
+pub extern fn exist_val__(env:&code_::Env_) -> Result2_ {
+	for_val__(env, |s, val| s == val, false)
+}
+#[no_mangle]
+pub extern fn first_exist_val__(env:&code_::Env_) -> Result2_ {
+	for_val__(env, |s, val| s == val, true)
+}
+#[no_mangle]
+pub extern fn cntn_val__(env:&code_::Env_) -> Result2_ {
+	for_val__(env, |s, val| s.contains(val), false)
+}
+#[no_mangle]
+pub extern fn first_cntn_val__(env:&code_::Env_) -> Result2_ {
+	for_val__(env, |s, val| s.contains(val), true)
+}*/
+
+#[cfg(debug_assertions)]
+#[no_mangle]
+pub extern fn test__(env:&code_::Env_) -> Result2_ {
+	let q = as_ref__!(env.q);
+	let args = &as_ref__!(q.args_);
+	let a = &args.a_;
+	let mut idx = 6;
+	let mut val = String::new();
+	let mut ret = as_mut_ref__!(env.ret);
+	let w = as_ref__!(env.w);
+	as_ref__!(a[0]).s__(&mut val);
+	as_ref__!(a[2]).s__(&mut val);
+	as_ref__!(a[4]).s__(&mut val);
+	while idx < a.len() {
+		let i = as_ref__!(a[idx]);
+		let b = i.dunhao__();
+		if !b {
+			i.s__(&mut val);
+		}
+		idx += 1;
+		let mut b2 = idx >= a.len();
+		if b || b2 {
+			w.dunhao__(&mut ret);
+			ret.add__(val.clone());
+			if(b2) {break;}
 			val.clear();
 		}
 	}
